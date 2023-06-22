adbbf456793de470db3df7ac1e526b5f50f49ddc
thuanWorkspace <117074155+thuanWorkspace@users.noreply.github.com>
2023-06-22 22:07:37 +0700

    first commit

---
+++ src/test/java/com/excercise/lab7/De1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excercise.lab7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class De1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
